616a624b7d2da85c3413f3c4ff2834d35bc1c7bc
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-10-15 15:30:01 -0300

    feat: user auth

---
+++ src/common/authVerify.ts
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import { withRouter } from "./withRouter";
+
+const parseJwt = (token:string) => {
+  try {
+    return JSON.parse(atob(token.split('.')[1]));
+  } catch (e) {
+    return null;
+  }
+};
+
+const AuthVerify = (props:any) => {
+  let location = props.router.location;
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (user) {
+      const decodedJwt = parseJwt(user.accessToken);
+
+      if (decodedJwt.exp * 1000 < Date.now()) {
+        props.logOut();
+      }
+    }
+  }, [location]);
+
+  return <div></div>;
+};
+
+export default withRouter(AuthVerify);
